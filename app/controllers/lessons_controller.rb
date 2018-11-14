92cabfe173dd6dff084066ec61b24be4a69a232b
David Lineman <dline215@gmail.com>
2018-11-14 19:19:06 +0000

    added a enrolled layer to permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :user_enrolled?
+
   def show
   end
 
   private 
+
+  def user_enrolled?
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to :back, alert: 'You are not enrolled!'
+    end
+  end
+
 
   helper_method :current_lesson
   def current_lesson
