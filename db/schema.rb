6db427f39f6fb604ae05e369f7817697c9d2dd4b
David Lineman <dline215@gmail.com>
2018-11-28 16:52:35 +0000

    added basic video uploading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181114213439) do
+ActiveRecord::Schema.define(version: 20181128160020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20181114213439) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "row_order"
+    t.string   "video"
     t.index ["row_order"], name: "index_lessons_on_row_order", using: :btree
     t.index ["section_id"], name: "index_lessons_on_section_id", using: :btree
   end
